46a840252f5358fd419be5b9b7887b6704873e7a
eustaquio <eustaquio.galano@gmail.com>
2025-01-13 16:50:43 +0800

    refactor: create a function for validating password

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
+function checkMatchingPassword() {
+    if (password.value !== passwordConfirmation.value) {
+        matchingMessage.textContent = "Make sure to match the password.";
+        passwordConfirmation.classList.toggle('invalid');        
+    } else {
+        matchingMessage.textContent = "Password matched"
+        // passwordConfirmation.classList.remove("invalid");
+    }
+}
+
 const password = document.querySelector("#password");
 const passwordConfirmation = document.querySelector("#confirmation");
 const signUpForm = document.querySelector("#sign-up-form");
 const matchingMessage = document.querySelector("#matching-message");
 
 
-passwordConfirmation.addEventListener('input', () => {
-    if (password.value !== passwordConfirmation.value) {
-        matchingMessage.textContent = "Make sure to match the password.";
-        passwordConfirmation.classList.add('invalid');        
-    } else {
-        matchingMessage.textContent = "Password matched"
-        passwordConfirmation.classList.remove("invalid");
-    }
-});
+passwordConfirmation.addEventListener('input', checkMatchingPassword);
+password.addEventListener('input', checkMatchingPassword);
